fb9734d5ff384f6692597e8f551e292a60b3b2a0
Boris Krisanov <boriskrisanov1@gmail.com>
2024-11-01 20:59:33 +0000

    Initialise SVG renderers for pieces at startup

--- ui/src/BoardWidget.hpp
+++ ui/src/BoardWidget.hpp
@@ -3,9 +3,7 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QtSvg/QtSvg>
-#include <QtSvgWidgets/QtSvgWidgets>
 #include "Board.hpp"
-#include <iostream>
 
 class BoardWidget : public QWidget
 {
@@ -33,14 +31,9 @@ public:
 
                 if (!board.isSquareEmpty(index))
                 {
-                    QString pieceFileName = QString::fromStdString(board[index].toString()).toLower() + ".svg";
-                    if (board[index].color == WHITE)
-                    {
-                        pieceFileName.prepend("_");
-                    }
-                    pieceFileName.prepend("pieces/");
-                    QSvgRenderer svg{pieceFileName};
-                    svg.render(&painter, QRect{j * squareSize, i * squareSize, squareSize, squareSize});
+                    QSvgRenderer* pieceRenderer = getPieceRenderer(board[index]);
+                    const QRect piecePosition{j * squareSize, i * squareSize, squareSize, squareSize};
+                    pieceRenderer->render(&painter, piecePosition);
                 }
             }
             squareColor = squareColor == lightSquareColor ? darkSquareColor : lightSquareColor;
@@ -48,8 +41,69 @@ public:
     }
 
 private:
+    QSvgRenderer* getPieceRenderer(Piece piece) const
+    {
+        if (piece.color == WHITE)
+        {
+            switch (piece.kind)
+            {
+            case PieceKind::PAWN:
+                return whitePawnRenderer;
+            case PieceKind::KNIGHT:
+               return whiteKnightRenderer;
+            case PieceKind::BISHOP:
+                return whiteBishopRenderer;
+            case PieceKind::ROOK:
+                return whiteRookRenderer;
+            case PieceKind::QUEEN:
+                return whiteQueenRenderer;
+            case PieceKind::KING:
+                return whiteKingRenderer;
+            default:
+                break;
+            }
+        }
+        else
+        {
+            switch (piece.kind)
+            {
+            case PieceKind::PAWN:
+                return blackPawnRenderer;
+            case PieceKind::KNIGHT:
+                return blackKnightRenderer;
+            case PieceKind::BISHOP:
+                return blackBishopRenderer;
+            case PieceKind::ROOK:
+                return blackRookRenderer;
+            case PieceKind::QUEEN:
+                return blackQueenRenderer;
+            case PieceKind::KING:
+                return blackKingRenderer;
+            default:
+                break;
+            }
+        }
+        return {};
+    }
+
     const QColor lightSquareColor{240, 217, 181};
     const QColor darkSquareColor{181, 136, 99};
-    const int squareSize = 50;
+    const int squareSize = 70;
     Board board;
+
+    // Piece renderers
+    // Initialising a renderer for each piece once avoids reloading the SVGs each time the board is rendered
+
+    QSvgRenderer* whitePawnRenderer = new QSvgRenderer{QString("pieces/_p.svg")};
+    QSvgRenderer* whiteKnightRenderer = new QSvgRenderer{QString("pieces/_n.svg")};
+    QSvgRenderer* whiteBishopRenderer = new QSvgRenderer{QString("pieces/_b.svg")};
+    QSvgRenderer* whiteRookRenderer = new QSvgRenderer{QString("pieces/_r.svg")};
+    QSvgRenderer* whiteQueenRenderer = new QSvgRenderer{QString("pieces/_q.svg")};
+    QSvgRenderer* whiteKingRenderer = new QSvgRenderer{QString("pieces/_k.svg")};
+    QSvgRenderer* blackPawnRenderer = new QSvgRenderer{QString("pieces/p.svg")};
+    QSvgRenderer* blackKnightRenderer = new QSvgRenderer{QString("pieces/n.svg")};
+    QSvgRenderer* blackBishopRenderer = new QSvgRenderer{QString("pieces/b.svg")};
+    QSvgRenderer* blackRookRenderer = new QSvgRenderer{QString("pieces/r.svg")};
+    QSvgRenderer* blackQueenRenderer = new QSvgRenderer{QString("pieces/q.svg")};
+    QSvgRenderer* blackKingRenderer = new QSvgRenderer{QString("pieces/k.svg")};
 };
