70f4d04e758f31e148a03c3c445bfd10a7fd299a
Boris Krisanov <boriskrisanov1@gmail.com>
2024-11-03 13:21:51 +0000

    Add piece movement and refactor piece rendering to use QSvgWidget

--- ui/src/BoardWidget.hpp
+++ ui/src/BoardWidget.hpp
@@ -3,7 +3,9 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QtSvg/QtSvg>
+#include <QSvgWidget>
 #include "Board.hpp"
+#include <cmath>
 
 class BoardWidget : public QWidget
 {
@@ -14,6 +16,28 @@ public:
         : QWidget(parent)
     {
         board.loadFen(STARTING_POSITION_FEN);
+
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                const int index = 8 * i + j;
+                if (board.isSquareEmpty(index))
+                {
+                    continue;
+                }
+                QString fileName = QString::fromStdString(board[index].toString()).toLower() + ".svg";
+                if (board[index].color == WHITE)
+                {
+                    fileName.prepend("_");
+                }
+                fileName.prepend("pieces/");
+                auto* piece = new QSvgWidget(fileName, this);
+                pieceWidgets[index] = piece;
+                piece->setGeometry(j * squareSize, i * squareSize, squareSize, squareSize);
+                piece->show();
+            }
+        }
     }
 
     void paintEvent(QPaintEvent* event) override
@@ -28,82 +52,84 @@ public:
                 squareColor = squareColor == lightSquareColor ? darkSquareColor : lightSquareColor;
                 QRect square(j * squareSize, i * squareSize, squareSize, squareSize);
                 painter.fillRect(square, squareColor);
-
-                if (!board.isSquareEmpty(index))
-                {
-                    QSvgRenderer* pieceRenderer = getPieceRenderer(board[index]);
-                    const QRect piecePosition{j * squareSize, i * squareSize, squareSize, squareSize};
-                    pieceRenderer->render(&painter, piecePosition);
-                }
             }
             squareColor = squareColor == lightSquareColor ? darkSquareColor : lightSquareColor;
         }
     }
 
-private:
-    QSvgRenderer* getPieceRenderer(Piece piece) const
+    void mouseMoveEvent(QMouseEvent* event) override
     {
-        if (piece.color == WHITE)
+        // Called when mouse is moved while pressed
+        if (!isPieceBeingMoved)
         {
-            switch (piece.kind)
-            {
-            case PieceKind::PAWN:
-                return whitePawnRenderer;
-            case PieceKind::KNIGHT:
-               return whiteKnightRenderer;
-            case PieceKind::BISHOP:
-                return whiteBishopRenderer;
-            case PieceKind::ROOK:
-                return whiteRookRenderer;
-            case PieceKind::QUEEN:
-                return whiteQueenRenderer;
-            case PieceKind::KING:
-                return whiteKingRenderer;
-            default:
-                break;
-            }
+            return;
         }
-        else
+        const QPoint mousePosition = event->pos();
+        QSvgWidget* pieceWidget = pieceWidgets[moveStartIndex];
+        // Subtract half of square size so that the cursor is in the middle of the piece
+        const QPoint position = mousePosition - QPoint{squareSize / 2, squareSize / 2};
+        pieceWidget->move(position);
+    }
+
+    void mousePressEvent(QMouseEvent* event) override
+    {
+        if (isPieceBeingMoved)
         {
-            switch (piece.kind)
-            {
-            case PieceKind::PAWN:
-                return blackPawnRenderer;
-            case PieceKind::KNIGHT:
-                return blackKnightRenderer;
-            case PieceKind::BISHOP:
-                return blackBishopRenderer;
-            case PieceKind::ROOK:
-                return blackRookRenderer;
-            case PieceKind::QUEEN:
-                return blackQueenRenderer;
-            case PieceKind::KING:
-                return blackKingRenderer;
-            default:
-                break;
-            }
+            // Probably not needed
+            return;
         }
-        return {};
+        const int index = coordinatesToBoardIndex(event->pos());
+        if (board[index].isNone())
+        {
+            return;
+        }
+        isPieceBeingMoved = true;
+        moveStartIndex = index;
+        pieceBeingMoved = board[index];
+    }
+
+    void mouseReleaseEvent(QMouseEvent* event) override
+    {
+        isPieceBeingMoved = false;
+        const QPoint mousePosition = event->pos();
+        const int newIndex = coordinatesToBoardIndex(mousePosition);
+
+        if (pieceWidgets[newIndex] != nullptr)
+        {
+            delete pieceWidgets[newIndex];
+        }
+
+        pieceWidgets[newIndex] = pieceWidgets[moveStartIndex];
+        pieceWidgets[moveStartIndex] = nullptr;
+        pieceWidgets[newIndex]->move(boardIndexToCoordinates(newIndex));
+        // TODO: Castling, promotion, en passant, etc)
+        board.makeMove(Move{static_cast<Square>(moveStartIndex), static_cast<Square>(newIndex), MoveFlag::None});
+    }
+
+private:
+    int coordinatesToBoardIndex(QPoint coordinates) const
+    {
+        int file = std::floor(coordinates.x() / static_cast<double>(squareSize));
+        int rank = std::floor(coordinates.y() / static_cast<double>(squareSize));
+
+        return 8 * rank + file;
+    }
+
+    QPoint boardIndexToCoordinates(int index)
+    {
+        int file = index % 8;
+        int rank = std::floor(index / 8);
+
+        return {squareSize * file, squareSize * rank};
     }
 
     const QColor lightSquareColor{240, 217, 181};
     const QColor darkSquareColor{181, 136, 99};
     const int squareSize = 70;
     Board board;
+    std::array<QSvgWidget*, 64> pieceWidgets;
 
-    // Piece renderers
-    // Initialising a renderer for each piece once avoids reloading the SVGs each time the board is rendered
-
-    QSvgRenderer* whitePawnRenderer = new QSvgRenderer{QString("pieces/_p.svg")};
-    QSvgRenderer* whiteKnightRenderer = new QSvgRenderer{QString("pieces/_n.svg")};
-    QSvgRenderer* whiteBishopRenderer = new QSvgRenderer{QString("pieces/_b.svg")};
-    QSvgRenderer* whiteRookRenderer = new QSvgRenderer{QString("pieces/_r.svg")};
-    QSvgRenderer* whiteQueenRenderer = new QSvgRenderer{QString("pieces/_q.svg")};
-    QSvgRenderer* whiteKingRenderer = new QSvgRenderer{QString("pieces/_k.svg")};
-    QSvgRenderer* blackPawnRenderer = new QSvgRenderer{QString("pieces/p.svg")};
-    QSvgRenderer* blackKnightRenderer = new QSvgRenderer{QString("pieces/n.svg")};
-    QSvgRenderer* blackBishopRenderer = new QSvgRenderer{QString("pieces/b.svg")};
-    QSvgRenderer* blackRookRenderer = new QSvgRenderer{QString("pieces/r.svg")};
-    QSvgRenderer* blackQueenRenderer = new QSvgRenderer{QString("pieces/q.svg")};
-    QSvgRenderer* blackKingRenderer = new QSvgRenderer{QString("pieces/k.svg")};
+    bool isPieceBeingMoved = false;
+    int moveStartIndex;
+    Piece pieceBeingMoved;
 };
